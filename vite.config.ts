4bc9869836883efb3c4dbe6544a9be5b659bf57c
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-05-04 23:37:37 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@crate/",
+        replacement: "/crates/",
+      },
+    ],
+  },
   plugins: [react()],
   define: {
     "process.env": {},
   },
+  base: "./",
 });
